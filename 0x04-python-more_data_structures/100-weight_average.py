0ef23901b8a93226c04703e19d039cc5c5b6ca2c
Speedmob <oooamalzagh@hotmail.com>
2023-12-06 13:59:56 -0800

    0x04-python-more_data_structures-13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    total, num = 0, 0
+    for i in my_list:
+        total += i[0] * i[1]
+        num += i[1]
+    return float(total / num)
