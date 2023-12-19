b4f20d3ebfbe83b901c5391f1822a168bbe9ae92
Speedmob <oooamalzagh@hotmail.com>
2023-12-19 09:14:05 -0800

    0x06-python-classes-2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Define a square class."""
+
+
+class Square:
+    """a square."""
+    def __init__(self, size):
+        """a Square initializator.
+
+        Args:
+            size (int): size of square.
+        """
+        self.__size = size
