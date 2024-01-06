38bef862b6d308b9ffc8d7e72ffa4853ad518d0a
Speedmob <oooamalzagh@hotmail.com>
2024-01-06 10:18:49 -0800

    0x07-python-test_driven_development-3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a square-printing function."""
+
+
+def print_square(size):
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for x in range(size):
+        for y in range(size):
+            print("#", end="")
+        print("")
