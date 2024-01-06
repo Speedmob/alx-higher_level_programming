c5ade1878854e354e2a2dce364d6d7e4ec6410e2
Speedmob <oooamalzagh@hotmail.com>
2024-01-06 07:53:03 -0800

    0x08-python-more_classes-4

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """A class that defines a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new Rectangle
+
+        Args:
+           width (int): Width of Rectange
+           height (int): Height of Rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if not value >= 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if not value >= 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        if not self.__width or not self.__height:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        if not self.__width or not self.__height:
+            return ("")
+        rectstr = []
+        for x in range(self.__height):
+            for y in range(self.__width):
+                rectstr.append('#')
+            if x != self.__height - 1:
+                rectstr.append("\n")
+        return ("".join(rectstr))
